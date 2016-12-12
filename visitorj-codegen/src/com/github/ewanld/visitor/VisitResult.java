979bffeb56bc93404c50e9b1129199b88a3e092b
Ewan Lassudrie-Duchesne <ewan.lassudrieduchesne@atos.net>
2016-12-12 15:06:28 +0100

    add javadoc

--- visitorj-codegen/src/com/github/ewanld/visitor/VisitResult.java
+++ visitorj-codegen/src/com/github/ewanld/visitor/VisitResult.java
@@ -1,4 +1,24 @@
 package com.github.ewanld.visitor;
+
+/**
+ * Represent the next step to be taken by a Visitor after visiting a node.
+ */
 public enum VisitResult {
-	CONTINUE, ABORT, SKIP_CHILDREN, SKIP_SIBLINGS
+	/**
+	 * The Visitor should continue the normal traversal of the tree.
+	 */
+	CONTINUE,
+	/**
+	 * The Visitor should exit immediatly.
+	 */
+	ABORT,
+	/**
+	 * The Visitor should skip the children of the visited node.
+	 */
+	SKIP_CHILDREN,
+
+	/**
+	 * The Visitor should skip the siblings of the visited node.
+	 */
+	SKIP_SIBLINGS
 }
