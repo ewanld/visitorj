bce761228dac5f2d9530d227e397048a4c787726
Ewan Lassudrie-Duchesne <ewan.lassudrieduchesne@atos.net>
2018-02-10 15:33:53 +0100

    add plugin parameters

--- visitorj-codegen-maven-plugin/src/main/java/com/github/visitorj/CodeGeneratorMojo.java
+++ visitorj-codegen-maven-plugin/src/main/java/com/github/visitorj/CodeGeneratorMojo.java
@@ -1,5 +1,11 @@
 package com.github.visitorj;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -7,16 +13,37 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import com.github.visitorj.codegen.GeneratorType;
+import com.github.visitorj.codegen.JavaClass;
+import com.github.visitorj.codegen.VisitorGeneratorService;
+
 /**
  * This goal will say a message.
  */
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES, threadSafe = true)
 public class CodeGeneratorMojo extends AbstractMojo {
+	@Parameter(property = "visitorName", defaultValue = "Visitor")
+	private String visitorName;
+
+	@Parameter(property = "outputDirectory", defaultValue = "${project.build.directory}/generated/java")
+	private String outputDirectory;
+
 	@Parameter(property = "packageName", defaultValue = "")
 	private String packageName;
 
+	@Parameter(property = "classes")
+	private List<String> classes;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().info("hi there");
+		getLog().info("Generating sources for visitor " + visitorName);
+		final VisitorGeneratorService service = new VisitorGeneratorService();
+		final List<JavaClass> javaClasses = classes.stream().map(JavaClass::new).collect(Collectors.toList());
+		try {
+			service.generate(visitorName, new File(outputDirectory), javaClasses, packageName,
+					EnumSet.allOf(GeneratorType.class));
+		} catch (final IOException e) {
+			throw new MojoExecutionException(e.getMessage(), e);
+		}
 	}
 }
