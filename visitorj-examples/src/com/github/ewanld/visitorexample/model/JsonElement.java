6ed77c2306c256589e98cbea19e224b939b6a113
Ewan Lassudrie-Duchesne <ewan.lassudrieduchesne@atos.net>
2018-02-10 15:51:10 +0100

    move package

--- visitorj-examples/src/com/github/ewanld/visitorexample/model/JsonElement.java
+++
@@ -1,8 +0,0 @@
-package com.github.ewanld.visitorexample.model;
-
-import com.github.ewanld.visitor.Visitable;
-import com.github.ewanld.visitorexample.model.JsonVisitor;
-
-public interface JsonElement extends Visitable<JsonVisitor> {
-
-}
