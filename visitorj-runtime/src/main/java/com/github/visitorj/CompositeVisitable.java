ff091572e2584c8a3db96c814ba32a2b4e0987da
Ewan Lassudrie-Duchesne <ewan.lassudrieduchesne@atos.net>
2018-03-09 10:46:27 +0100

    simpler interface

--- visitorj-runtime/src/main/java/com/github/visitorj/CompositeVisitable.java
+++ visitorj-runtime/src/main/java/com/github/visitorj/CompositeVisitable.java
@@ -1,15 +1,14 @@
 package com.github.visitorj;
 
 public abstract class CompositeVisitable<V> implements Visitable<V> {
-	private final Iterable<IdentifiedVisitable<V>> children;
+	private final VisitableList<V> children = new VisitableList<>();
 
-	public CompositeVisitable(Iterable<IdentifiedVisitable<V>> children) {
-		this.children = children;
+	public CompositeVisitable(Iterable<? extends Visitable<V>> iterable) {
+		children.add(iterable);
 	}
 
 	@Override
 	public Iterable<IdentifiedVisitable<V>> getVisitableChildren() {
 		return children;
 	}
-
 }
